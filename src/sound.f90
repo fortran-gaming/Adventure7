3531634fe23477098b10c8972cdcf26b12ff7327
scivision <scivision@users.noreply.github.com>
2017-06-11 14:33:15 -0400

    ig

--- src/sound.f90
+++ src/sound.f90
@@ -16,7 +16,8 @@ SUBROUTINE SOUND(WHAT)
   cmd = playexe//' '//cmdopts//' '//fn
   if (debug) print *,cmd
 
-  call execute_command_line(cmd, exitstat=ret)
+  ! exitstat only works for wait=.true. by Fortran 2008 spec.
+  call execute_command_line(cmd, wait=.true.,exitstat=ret)
   if (debug) print *,'sound player return code',ret
 
   if (ret/=0) then
