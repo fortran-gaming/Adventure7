62ad6309338ad4325d7a2699adb3e658b5e1c74f
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-05-09 13:22:20 -0400

    init

---
+++ src/bug_legacy.f90
@@ -0,0 +1,11 @@
+SUBROUTINE BUG(MSG)
+use, intrinsic:: iso_fortran_env, only: error_unit
+implicit none
+
+
+CHARACTER(*), Intent(in) :: MSG
+
+WRITE(error_unit,*) msg
+
+stop 'FATAL ERROR'
+END Subroutine Bug
