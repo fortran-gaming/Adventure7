bacf1ad3bae198e2db891c47d4d9ab5077be3aac
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2021-01-27 00:40:11 -0500

    cleanup

--- cmake/compilers.cmake
+++ cmake/compilers.cmake
@@ -1,8 +1,9 @@
 if(CMAKE_Fortran_COMPILER_ID STREQUAL GNU)
-  add_compile_options(-mtune=native -Wall -Wextra -Werror=array-bounds -fcheck=all
+  add_compile_options(-fall-intrinsics -mtune=native -Wall -Wextra -Wline-truncation -fimplicit-none)
+
+  set(CMAKE_Fortran_FLAGS_DEBUG " -Werror=array-bounds -fcheck=all
   -fexceptions -ffpe-trap=invalid,zero,overflow
-  -finit-real=nan -Wconversion -fimplicit-none -fall-intrinsics
-  -Wline-truncation  -Wno-unused-dummy-argument) #-Warray-temporaries
+  -finit-real=nan -Wconversion -Wno-unused-dummy-argument") #-Warray-temporaries
 
   if(CMAKE_Fortran_COMPILER_VERSION VERSION_GREATER_EQUAL 8)
     add_compile_options(-std=f2018)
@@ -11,13 +12,7 @@ elseif(CMAKE_Fortran_COMPILER_ID STREQUAL Intel)
   if(WIN32)
 
   else()
-  add_compile_options(-warn -fpe0 -traceback -debug extended -check all -implicitnone)
+    add_compile_options(-warn -traceback)
+    set(CMAKE_Fortran_FLAGS_DEBUG " -fpe0 -debug extended -check all")
   endif()
 endif()
-
-include(CheckFortranSourceCompiles)
-
-check_fortran_source_compiles("program cm; call execute_command_line(''); end" f2008
-                              SRC_EXT f90)
-
-
